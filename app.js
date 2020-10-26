20865fbf960e9caf59e33e87d2042bf38d931d29
achrysaetos <achrysaetos@yahoo.com>
2020-10-25 22:31:47 -0500

    personalized

--- app.js
+++ app.js
@@ -46,33 +46,6 @@ app.use(session({
     }
 }));//to use express-session
 
-// Set your secret key. Remember to switch to your live secret key in production!
-// See your keys here: https://dashboard.stripe.com/account/apikeys
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
-
-app.post("/create-checkout-session", async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: "Payment Amount",
-          },
-          unit_amount: 2000,
-        },
-        quantity: 1,
-      },
-    ],
-    mode: "payment",
-    success_url: "http://localhost:3000/",
-    cancel_url: "http://localhost:3000/",
-  });
-
-  res.json({ id: session.id });
-});
-
 app.use('/', indexRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -17,9 +17,4 @@ router.post("/logout", goto.logout);
 router.get("/dashboard", goto.dashboard);
 router.get("/logout", goto.logout);
 
-router.post("/payments", goto.login_post);
-router.get("/payments", goto.payments);
-router.post("/history", goto.login_post);
-router.get("/history", goto.history);
-
 module.exports = router;
