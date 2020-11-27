0cfea796b5965200dcd19527da613f5923bd3dfc
achrysaetos <51834103+achrysaetos@users.noreply.github.com>
2020-11-26 22:46:36 -0600

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var helmet = require('helmet');//after we npm install helmet
 
 //Connects to MongoDB
 var mongoose = require("mongoose");//imports the mongoose module
-var dev_db_url = "mongodb+srv://achrysaetos:sempiternal@cluster0-jjeek.mongodb.net/fullstack_template?retryWrites=true&w=majority"
+var dev_db_url = "";//database url goes here
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });//sets up the defualt mongoose connection
 var db = mongoose.connection;//gets the default connection
